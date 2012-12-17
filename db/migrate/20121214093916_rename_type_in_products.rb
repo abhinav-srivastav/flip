4aa70940443ab36b091622548d7384e11705db84
Abhinav Srivastav <abhinav@abhinav-HP-ProBook-4530s.(none)>
2012-12-17 17:36:45 +0530

    removed unwanted migration

--- db/migrate/20121214093916_rename_type_in_products.rb
+++
@@ -1,9 +0,0 @@
-class RenameTypeInProducts < ActiveRecord::Migration
-  def up
-  	rename_column :products, :type, :product_type
-  end
-
-  def down
-  	rename_column :products, :product_type, :type
-  end
-end
