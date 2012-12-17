f2be1cf1b9da66ba09983a0b98455a4c2d24aef5
Abhinav Srivastav <abhinav@abhinav-HP-ProBook-4530s.(none)>
2012-12-17 17:35:46 +0530

    added normal user's edit and sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,44 @@
 class UsersController < ApplicationController
   layout 'public'
   skip_before_filter :admin_authorize
-  before_filter :user_authorize
+  before_filter :user_authorize, :except => [:new, :create]
   def show
   	@user = User.find(params[:id])
   	respond_to do |format|
   	  format.html
   	end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create 
+    @user  = User.new(params[:user])
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to :root }
+      else
+        format.html {render action: 'new'}
+      end
+    end
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  	unless @user.id.to_i == current_user.id.to_i
+  	  redirect_to :root, notice: 'Not authorized.'
+  	end 
+  end
+
+  def update
+  	@user = User.find(params[:id])
+	  respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to user_path(@user) }
+      else
+        format.html { render action: "edit"}
+      end
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   attr_accessible :username, :password, :password_confirmation, :email, :wallet
   has_secure_password
+  validates :username, presence: true
+  
+
+  private
 
   def self.authorize(username, password)
     user = User.find_by_username(username)
