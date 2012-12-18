0fd932c9ed3ceb15245c6a7689592cf53cf6c745
Abhinav Srivastav <abhinav@abhinav-HP-ProBook-4530s.(none)>
2012-12-18 20:30:30 +0530

    removed unwanted model trolley

--- app/models/trolley.rb
+++
@@ -1,5 +0,0 @@
-class Trolley < ActiveRecord::Base
-  has_and_belongs_to_many :products
- 
-  # attr_accessible :title, :body
-end
--- db/migrate/20121217134745_create_products_trolleys.rb
+++
@@ -1,8 +0,0 @@
-class CreateProductsTrolleys < ActiveRecord::Migration
-  def change
-    create_table :products_trolleys, :id => false do |t|
-      t.integer 'product_id'
-      t.integer 'trolley_id'
-    end
-  end
-end
