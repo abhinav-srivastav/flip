4c545e03a635242b4cea41592efb0446c9659565
Abhinav Srivastav <abhinav@abhinav-HP-ProBook-4530s.(none)>
2013-01-17 12:06:02 +0530

    modified category show page

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
   belongs_to :product
   attr_accessible :image
-  has_attached_file :image, :styles => { :thumb => "200x200>", :small => "70x70>"},
+  has_attached_file :image, :styles => { :thumb => "300x300>", :small => "150x150>"},
           	    					  :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
  	  			           			  :url => "/system/:attachment/:id/:style/:filename"
   validates_attachment_presence :image
