4ab07880b240546ba9b29f6fbb2bf89ddfd1b759
Abhinav Srivastav <abhinav@abhinav-HP-ProBook-4530s.(none)>
2013-01-28 15:49:32 +0530

    minor fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,11 @@ class User < ActiveRecord::Base
   has_many :transactions
   attr_accessible :username, :password, :admin, :super, :password_confirmation, :email, :wallet
 
-  # has_secure_password
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, :if => :password
   validates :email, uniqueness: true
   validates :wallet, :numericality => { :greater_than => 0   }
   
-  # [FIXME_CR] No need to add users with below scopes
-  # Thease are defined for a user itself.
   scope :normals, where(:super => false)
 
   scope :supers, where(:super => true)
@@ -34,22 +31,6 @@ class User < ActiveRecord::Base
 
   def add_to_cart(order_id)
     orders.cart_state.add_line_item_from_order(order_id)
-  end
-
-  private
-
-  def self.credit_to_admin(amount)
-    admin = super_user
-    admin.wallet += amount
-    return true if admin.save
-    return false     
-  end
-
-  def self.debit_from_admin(amount)
-    admin = super_user
-    admin.wallet -= amount
-    return true if admin.save
-    return false
   end
 
   def self.super_user
